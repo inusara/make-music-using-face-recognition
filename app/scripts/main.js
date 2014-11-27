e7a192b32aa7f6cd283522a72e40ab286e83a7f4
inusara <timvazquez@gmail.com>
2014-11-28 01:02:07 +0800

    now using custom tracker for per frame access, control and manipulation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,58 @@
 window.onload = function() {
-	var video = document.getElementById('video'); 
+	'use strict';
 	var canvas = document.getElementById('canvas');
 	var context = canvas.getContext('2d');
 	var guiInterface = {
 		isTracking: false
 	};
 
-	var faceTracker = new tracking.ObjectTracker('face');
-	faceTracker.setInitialScale(4);
-	faceTracker.setStepSize(2);
-	faceTracker.setEdgesDensity(0.1);
+	var FastTracker = function() {
+		FastTracker.base(this, 'constructor');
+	};
+	tracking.inherits(FastTracker, tracking.Tracker);
 
-	tracking.track('#video', faceTracker, { camera: true });
+	tracking.Fast.THRESHOLD = 1;
+	FastTracker.prototype.threshold = tracking.Fast.THRESHOLD;
 
-	function process(event) {
-		clearCanvas();
+	var face = new tracking.ObjectTracker('face');
+		face.setInitialScale(4);
+		face.setStepSize(2);
+		face.setEdgesDensity(0.1);
+
+	FastTracker.prototype.track = function(pixels, width, height) {
+		var classifiers = face.getClassifiers();
+
+	    if (!classifiers) {
+	      throw new Error('Object classifier not specified, try `new tracking.ObjectTracker("face")`.');
+	    }
+
+	    var results = [];
+
+	    classifiers.forEach(function(classifier) {
+	      results = results.concat(tracking.ViolaJones.detect(pixels, width, height, face.getInitialScale(), face.getScaleFactor(), face.getStepSize(), face.getEdgesDensity(), classifier));
+	    });
+
+		this.emit('track', {
+			data: results
+		});
+	};
+
+	var tracker = new FastTracker();
+	var trackerTask = tracking.track('#video', tracker, { camera: true }).stop();
+
+	tracker.on('track', function(event) {
+		clearCanvas();		
 		if(event.data.length > 0) {
 			event.data.forEach(function(rect) {
-				context.strokeStyle = "#a64ceb";
+				context.strokeStyle = '#a64ceb';
 				context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+				context.font = '11px Helvetica';
+				context.fillStyle = '#fff';
+				context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+				context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
 			});
 		}
-	}
+	});
 
 	function clearCanvas() {
 		context.clearRect(0, 0, canvas.width, canvas.height);
@@ -30,10 +61,10 @@ window.onload = function() {
 	var gui = new dat.GUI();
 	gui.add(guiInterface, 'isTracking').onChange(function(value) {
 		if(value) {
-			faceTracker.addListener('track', process);
+			trackerTask.run();
 		} else {
-			faceTracker.removeListener('track', process);
+			trackerTask.stop();
 			clearCanvas();
 		}
 	});
-}
+};
