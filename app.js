54507af4b3456ef42e9c1c17d342ae19d2b498d7
inusara <timvazquez@gmail.com>
2014-12-01 16:36:03 +0800

    for heroku deployment of static files

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+	var express = require('express');
+	var app = express();
+	 
+	app.set('port', process.env.PORT || 3000);
+	 
+	app.use(express.logger('dev'));
+	app.use(express.compress());
+	app.use(express.static(__dirname + '/dist'));
+	 
+	app.listen(app.get('port'), function() {
+	});
+})();
